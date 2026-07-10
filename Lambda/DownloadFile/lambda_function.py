b2f795a4a37d629ee8c4258127d971a9b2236e16
Kuldeep Jangra <kuldeepjangra878@gmail.com>
2026-07-10 17:19:09 +0530

    Create DownloadFile

---
+++ Lambda/DownloadFile/lambda_function.py
@@ -0,0 +1,12 @@
+import boto3
+
+s3 = boto3.client('s3')
+BUCKET = "bucket_name"
+
+def lambda_handler(event, context):
+    url = s3.generate_presigned_url(
+        'get_object',
+        Params={'Bucket': BUCKET, 'Key': event['s3Key']},
+        ExpiresIn=60
+    )
+    return {"downloadURL": url}
